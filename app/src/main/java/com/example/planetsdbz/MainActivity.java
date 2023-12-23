5745d44c1b37c73cbe0a32691605912427a66628
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-22 23:41:34 -0500

    planet vegeta added to list and text info also

--- app/src/main/java/com/example/planetsdbz/MainActivity.java
+++ app/src/main/java/com/example/planetsdbz/MainActivity.java
@@ -5,13 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     ListView listView;
+    ArrayList<Planet> planetArrayList;
+    private  static  AbuCustomAdapter abuCustomAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 1- AdapterView: a lisView
         listView = findViewById(R.id.listview);
+
+        // 2- Data Source
+        planetArrayList = new ArrayList<>();
+
+        Planet vegeta = new Planet("Vegeta", "9.8 * 100",R.drawable.planetvegeta);
+        planetArrayList.add(vegeta);
+
+        abuCustomAdapter = new AbuCustomAdapter(planetArrayList,getApplicationContext());
+        listView.setAdapter(abuCustomAdapter);
+
     }
 }
